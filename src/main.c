999da45fb873a39879937564286dcf5de0c89b38
theokrueger <theokrueger@airmail.cc>
2024-07-04 17:42:43 -0400

    queued button inputs working

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
 #include "pico/time.h"
-
+#include "pico/sync.h"
+#include "pico/multicore.h"
+#include "hardware/pwm.h"
 
 // pins
 #define P1_SHOCK 15
@@ -16,9 +17,16 @@ static uint p2_slice = 0;
 #define BLANK_LED 3
 
 #define TRIGGER_BTN 6
+static mutex_t trigger_btn_mutex;
+static bool trigger_btn_pressed = 0;
 #define PUMP_BTN 7
+static mutex_t pump_btn_mutex;
+static bool pump_btn_pressed = 0;
 #define SWAP_BTN 8
+static mutex_t swap_btn_mutex;
+static bool swap_btn_pressed = 0;
 
+// consts
 #define SHOCK_TIME 1000 // shock time in ms
 
 // set up shock collars
@@ -72,13 +80,73 @@ void shock_p2() {
 	gpio_put(P2_SHOCK_LED, 0);
 }
 
-// main
+// set up game buttons/leds
+void setup_game_io() {
+	// led setup
+	gpio_init(LIVE_LED);
+	gpio_set_dir(LIVE_LED, GPIO_OUT);
+	gpio_put(LIVE_LED, 0);
+
+	gpio_init(BLANK_LED);
+	gpio_set_dir(BLANK_LED, GPIO_OUT);
+	gpio_put(BLANK_LED, 0);
+
+	// button setup
+	gpio_init(TRIGGER_BTN);
+	gpio_set_dir(TRIGGER_BTN, GPIO_IN);
+	mutex_init(&trigger_btn_mutex);
+
+	gpio_init(PUMP_BTN);
+	gpio_set_dir(PUMP_BTN, GPIO_IN);
+	mutex_init(&pump_btn_mutex);
+
+	gpio_init(SWAP_BTN);
+	gpio_set_dir(SWAP_BTN, GPIO_IN);
+	mutex_init(&swap_btn_mutex);
+}
+
+// function called  by second core to allow queued inputs
+void input_loop() {
+	while(true) {
+		if (gpio_get(TRIGGER_BTN)) {
+			mutex_enter_blocking(&trigger_btn_mutex);
+			trigger_btn_pressed = true;
+			mutex_exit(&trigger_btn_mutex);
+		}
+	        if (gpio_get(PUMP_BTN)) {
+			mutex_enter_blocking(&pump_btn_mutex);
+			pump_btn_pressed = true;
+			mutex_exit(&pump_btn_mutex);
+		}
+		if (gpio_get(SWAP_BTN)) {
+			mutex_enter_blocking(&swap_btn_mutex);
+			swap_btn_pressed = true;
+			mutex_exit(&swap_btn_mutex);
+		}
+		sleep_ms(10);
+	}
+}
+// main loop
 int main() {
 	setup_shock();
+	setup_game_io();
+	multicore_launch_core1(input_loop);
+	// full game loop
 	while(true) {
-		shock_p1();
-		sleep_ms(1000);
-		shock_p2();
-		sleep_ms(1000);
+		// reset button states
+		mutex_enter_blocking(&trigger_btn_mutex);
+		trigger_btn_pressed = false;
+		mutex_exit(&trigger_btn_mutex);
+
+		mutex_enter_blocking(&pump_btn_mutex);
+		pump_btn_pressed = false;
+		mutex_exit(&pump_btn_mutex);
+
+		mutex_enter_blocking(&swap_btn_mutex);
+		swap_btn_pressed = false;
+		mutex_exit(&swap_btn_mutex);
+
+		// game vars
+		
 	}
 }
